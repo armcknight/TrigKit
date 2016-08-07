714a2ad826f006aaa8f7a68247c3d844c3870d11
Andrew McKnight <amcknight2718@gmail.com>
2016-08-07 15:53:44 -0400

    moving some definitions from the app logic

---
+++ TrigKit/Definitions.swift
@@ -0,0 +1,28 @@
+//
+//  Definitions.swift
+//  TrigKit
+//
+//  Created by Andrew McKnight on 8/7/16.
+//  Copyright © 2016 Andrew McKnight. All rights reserved.
+//
+
+import Foundation
+
+public enum Measure {
+    case Hypotenuse
+    case Chord
+
+    case Sine
+    case Cosine
+    case Tangent
+
+    case Secant
+    case Cosecant
+    case Cotangent
+
+    case Versine
+    case Coversine
+    
+    case Exsecant
+    case Excosecant
+}
